e1e5b648814ac528ba0515d8350d777008947c48
malachilee <malachilee6@gmail.com>
2018-07-12 11:09:35 -0400

    classes work

--- Seamus'sHappyHut/Models/Address.cs
+++ Seamus'sHappyHut/Models/Address.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace Seamus_sHappyHut.Models
 {
     public class Address
     {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Customer")]
+        public int StreetAddress { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
+
     }
 }
---
+++ Seamus'sHappyHut/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Seamus_sHappyHut.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public int CustomerPhone { get; set; }
+        public int EmployeeNum { get; set; }
+        public string EmployeeContactName { get; set; }
+        public int EmployeeContactNumber { get; set; }
+
+
+
+    }
+}
